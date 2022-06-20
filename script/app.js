194f5d73839402a7d5b0c0dcdadd33a9a847dcb6
rauldevilla <raul.devilla@gmail.com>
2022-06-20 14:47:28 -0500

    Toggle header background color

--- script/app.js
+++ script/app.js
@@ -1,9 +1,20 @@
 
+const BRAND_COLOR = '#0d0d3f';
+
 const hamburguer = document.querySelector('.header .nav-bar .nav-list .hamburguer');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
-const header = document.querySelector('.header .container');
+const header = document.querySelector('.header.container');
 
 hamburguer.addEventListener('click', () => {
     hamburguer.classList.toggle('active');
     mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+    var scrollPosition = window.scrollY;
+    if (scrollPosition > 250) {
+        header.style.backgroundColor = BRAND_COLOR;
+    } else {
+        header.style.backgroundColor = 'transparent';
+    }
 });
