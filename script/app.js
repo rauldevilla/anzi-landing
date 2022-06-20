9fa2e399040ea003fa298789b013651e60706661
rauldevilla <raul.devilla@gmail.com>
2022-06-20 15:02:32 -0500

    Finish UI without content

--- script/app.js
+++ script/app.js
@@ -3,6 +3,7 @@ const BRAND_COLOR = '#0d0d3f';
 
 const hamburguer = document.querySelector('.header .nav-bar .nav-list .hamburguer');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const mobile_menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
 
 hamburguer.addEventListener('click', () => {
@@ -17,4 +18,11 @@ document.addEventListener('scroll', () => {
     } else {
         header.style.backgroundColor = 'transparent';
     }
+});
+
+mobile_menu_item.forEach(item => {
+    item.addEventListener('click', () => {
+        hamburguer.classList.toggle('active');
+        mobile_menu.classList.toggle('active');
+    });
 });
